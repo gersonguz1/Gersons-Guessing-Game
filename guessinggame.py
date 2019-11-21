437294920d23e977b7361cda3432f0b9161aae79
Guzman Vallecillo <352455@mcpsmd.org>
2019-11-21 09:17:10 -0500

    while loop added

--- guessinggame.py
+++ guessinggame.py
@@ -1,7 +1,14 @@
-a = input("whats my number?")
-print(a)
-if (a == 10) :
-    print("correct!")
-elif () :
+guessCorrect = False
+while guessCorrect == False:
+    a = int(input("whats my number?"))
+    if (a == 10) :
+        print("correct!")
+        guessCorrect = True
+    elif (a < 10) :
+        print("its too lowww my friend try higher")
+        
+    elif (a > 10) :
+        print("its too high my friend try lower")
+    
     
 
